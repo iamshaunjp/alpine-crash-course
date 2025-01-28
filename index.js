105205e784fa9f545a0dd802667578002b6fe772
iamshaunjp <shaun@thenetninja.co.uk>
2025-01-28 17:32:11 +0000

    form-template

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 document.addEventListener('alpine:init', () => {
-  Alpine.data('counter', () => ({
-    count: 0,
-    name: 'Mario',
-
-    logCount() {
-      console.log('The count is currently', this.count)
-    }
-  }))
+  
 })
