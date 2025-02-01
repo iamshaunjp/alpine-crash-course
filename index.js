c13d3cd527f593d0c9fef4adb077e32d89c1e481
iamshaunjp <shaun@thenetninja.co.uk>
2025-02-01 14:27:52 +0000

    lesson-11

--- index.js
+++ index.js
@@ -1,47 +1,21 @@
 document.addEventListener('alpine:init', () => {
-  Alpine.data('signupForm', () => ({
-    username: '',
-    password: '',
-    passwordConfirm: '',
-    belt: '',
-    bio: '',
-    newsletter: true,
-    showPass: false,
-    errors: {},
-    beltChoices: ['black', 'white', 'red', 'orange', 'brown'],
-    success: false,
+  Alpine.data('demo', () => ({
+    posts: [],
+    bg: '',
 
-    validateForm() {
-      this.errors = {}
+    init() {
+      console.log('init')
 
-      if (this.username.length < 3) {
-        this.errors.username = 'Username must be at least 3 characters.'
-      }
-      if (this.password.length < 6) {
-        this.errors.password = 'Password must be at least 6 characters.'
-      }
-      if (this.password !== this.passwordConfirm) {
-        this.errors.passwordConfirm = 'Passwords do not match.'
-      }
-      if (!this.belt) {
-        this.errors.belt = 'Please select a belt color.'
-      }
-      if (this.bio.length < 10) {
-        this.errors.bio = 'Bio must be at least 10 characters.'
-      }
-
-    },
-
-    submitForm($event) {
-      this.validateForm()
-
-      if (Object.keys(this.errors).length === 0) {
-        console.log(this.username, this.password, this.belt, this.bio, this.newsletter)
-
-        $event.target.reset()
-
-        this.success = true
-      }
+      // fetch data
+      fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+        .then(response => response.json())
+        .then(data => this.posts = data)
+      
+      // watch data for changes
+      this.$watch('bg', (newbg, oldbg) => {
+        document.body.style.backgroundColor = newbg
+      })
     }
-  }))
-})
+
+  }));
+});
