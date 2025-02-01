2a47ac15b9566fa5b9bd2e14a6dfeeb798ac58bb
iamshaunjp <shaun@thenetninja.co.uk>
2025-02-01 14:28:44 +0000

    lesson-11-start

--- index.js
+++ index.js
@@ -3,19 +3,7 @@ document.addEventListener('alpine:init', () => {
     posts: [],
     bg: '',
 
-    init() {
-      console.log('init')
-
-      // fetch data
-      fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-        .then(response => response.json())
-        .then(data => this.posts = data)
-      
-      // watch data for changes
-      this.$watch('bg', (newbg, oldbg) => {
-        document.body.style.backgroundColor = newbg
-      })
-    }
+    
 
   }));
 });
